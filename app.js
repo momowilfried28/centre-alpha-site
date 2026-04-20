d55d3e8c8c2e9060b4c8673f5b688a73b7b800d4
momowilfried28 <2000843@cegepgranby.qc.ca>
2026-04-20 15:49:01 -0400

    fix port

--- app.js
+++ app.js
@@ -304,8 +304,8 @@ app.get("/evenements", (req, res) => {
     images,
   });
 });
-const PORT = Number(process.env.PORT) || 3000;
 
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
