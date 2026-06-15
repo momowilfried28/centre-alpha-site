ecbffa639b1a6ccac7a49a71477f8a20b2284c16
momowilfried28 <2000843@cegepgranby.qc.ca>
2026-06-15 10:31:45 -0400

    add Dev to Main

--- app.js
+++ app.js
@@ -366,6 +366,8 @@ app.get("/evenements", (req, res) => {
   });
 });
 
+
+
 const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
